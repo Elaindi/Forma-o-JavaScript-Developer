9987de2d69951d3670a4823af4e420fb4fac3c52
Elaine  Santana <elaindi@gmail.com>
2026-04-22 08:53:51 -0300

    Add files via upload

---
+++ 17 O Que é Um Array/aula.js
@@ -0,0 +1,7 @@
+const alunos = ['João', 'Maria', 'Pedro', 'Ana', 'Lucas'];
+
+alunos.push('Elaine'); //adicionar uma aluno novo no array
+alunos[5] = 'Carlos';
+alunos.push('teste');
+alunos.push(10);
+console.log(alunos);
