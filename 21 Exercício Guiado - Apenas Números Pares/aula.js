a9ba626c2d16512291eb128e87ca6784ea460db0
Elaine  Santana <elaindi@gmail.com>
2026-04-27 11:28:08 -0300

    Add files via upload

---
+++ 21 Exercício Guiado - Apenas Números Pares/aula.js
@@ -0,0 +1,11 @@
+// crie um programa que seja capaz de percorrer uma lista de números e imprima cada número par enccontrado
+
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i];
+    if (numero % 2 === 0) {
+        console.log(numero);
+    }
+}
